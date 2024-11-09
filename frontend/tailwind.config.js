4fb851f7af0965fab0ede90639fe2d1c7d0ee2d2
Prince Kumar <princekumar201926@gmail.com>
2024-11-09 14:58:29 +0530

    Added primary color

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,7 +6,21 @@ export default {
   ],
 
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary_color:{
+          dark_background:{
+            DEFAULT:"#000000",
+            rgb:"0, 0, 0",
+            hsl:"11, 100%, 0%"
+          },
+          light_background:"white",
+          button_default:"#DB4444",
+          button_light:"#3FB7EF",
+          
+        }
+      }
+    },
   },
   plugins: [],
 
